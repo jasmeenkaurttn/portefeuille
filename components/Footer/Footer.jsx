607a46df2995104a7e3a2ced2e435fbe41cbd16a
jasmeenkaurttn <jasmeen.kaur@tothenew.com>
2022-09-20 14:17:23 +0530

    Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Container, Row, Col } from "reactstrap"
+import classes from './footer.module.css'
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  )
+
+  const date = new Date();
+  const year = date.getFullYear()
+  return <footer>
+    <Container>
+      <Row>
+        {/* --------- footer  ----------- */}
+        <Col lg='6'>
+          <div className={`${classes.footer_creator}`}>
+            <h6> Copyright &copy; {year} - Developed by Jasmeen Kaur. &nbsp;
+             All right reserved. </h6>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  </footer>
 }
 
 export default Footer
